f931b1ebf00551ac484bbb145c25737fd94f80da
JerYoMat <jeremy.emata@gmail.com>
2019-06-04 04:42:15 -0400

    help built out with break and continue and some extra formatting

---
+++ Help3.java
@@ -0,0 +1,73 @@
+class Help3 {
+  public static void main(String args[])
+    throws java.io.IOException {
+      char choice, ignore;
+
+      for (;;) {
+        do {
+          System.out.println("Help on:");
+          System.out.println("\t1. if");
+          System.out.println("\t2. switch");
+          System.out.println("\t3. for");
+          System.out.println("\t4. while");
+          System.out.println("\t5. do-while");
+          System.out.println("\t6. break");
+          System.out.println("\t7. continue\n");
+          System.out.print("Enter a number between 1 and 7 or q to quit:\n");
+          choice = (char) System.in.read();
+          do {
+            ignore = (char) System.in.read();
+          } while (ignore != '\n');
+          /* The nested do is used to discard any unwanted characters as read inputs are line buffered and unless its between 1 and 5 you want to dump it. */
+          
+        } while ( choice < '1' | choice > '7' & choice != 'q');
+        
+        if (choice == 'q') {
+          System.out.println("-----Goodbye-----");
+          break;}
+        System.out.println("\n");
+        
+        switch(choice) {
+          case '1':
+            System.out.println("The if:\n");
+            System.out.println("if(condition) statement;");
+            System.out.print("else statement;");
+            break;
+          case '2':
+            System.out.println("The switch:\n");
+            System.out.println("switch(expression) {\n");
+            System.out.println("\tcase constant:");
+            System.out.println("\t\tstatement sequence");
+            System.out.println("\t\tbreak;");
+            System.out.println("\t //rinse repeat...");
+            System.out.println("\tdefault:\n...");
+            System.out.println("}");
+            break;
+          case '3':
+            System.out.println("The for: \n");
+            System.out.println("for(init; condition; iteration)");
+            System.out.println(" statement;");
+            break;
+          case '4':
+            System.out.println("The while: \n");
+            System.out.println("while(condition) statement;");
+            break;
+          case '5':
+            System.out.println("The do-while: \n");
+            System.out.println(" do {");
+            System.out.println("  statement;");
+            System.out.println("} while (condition);");
+            break;
+          case '6':
+            System.out.println("The break:\n");
+            System.out.println("break; or break label;");
+            break;
+          case '7':
+            System.out.println("The continue:\n");
+            System.out.println("continue; or continue label;");
+            break;
+        }
+        System.out.println("\n\n-------------------");
+      }
+  }
+}
